2d4d8c4c24f24e320033e103b6c4301c25143d1c
Baldauf Julia <jbaldauf@pcs.com>
2024-03-25 14:54:08 +0000

    adding packages

---
+++ app/backend/static/assets/NoPage-327d16e8.js
@@ -0,0 +1,2 @@
+import{bB as o}from"./vendor-c3fe08cd.js";function e(){return o.jsx("h1",{children:"404"})}e.displayName="NoPage";export{e as Component};
+//# sourceMappingURL=NoPage-327d16e8.js.map
